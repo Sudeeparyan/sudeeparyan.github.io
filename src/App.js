7638cd385a7a2c6ae8cb98455d0f48f8485e2c51
Sudeeparyan <sudeeparyang@gmail.com>
2023-07-15 14:12:40 +0530

    feat: V2 resume

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Navbar from './Components/Navbar/Navbar';
+
+
+
+
+function App() {
+  return (
+    <>
+    <div className="App">
+
+      <Navbar/>
+    </div>
+    </>
+    
+  );
+}
+
+export default App;
+
+
